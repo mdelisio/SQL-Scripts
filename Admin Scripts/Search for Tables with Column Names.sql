d0de4c8e8cc86622e17a39c7eda0f16ea05bf7e1
Michael DeLisio <mdelisio@terrasmart.com>
2023-08-03 13:38:52 -0400

    P&M order draft and move some files

--- Admin Scripts/Search for Tables with Column Names.sql
+++ Admin Scripts/Search for Tables with Column Names.sql
@@ -12,9 +12,13 @@ SELECT      COLUMN_NAME AS 'ColumnName'
             ,TABLE_NAME AS  'TableName'
             ,TABLE_SCHEMA as 'SchemaName'
 FROM        INFORMATION_SCHEMA.COLUMNS
-WHERE       COLUMN_NAME LIKE 'division%'
-ORDER BY    TableName
-            ,ColumnName;
+WHERE       COLUMN_NAME LIKE 'ship%'
+and         Table_Schema = 'erp'
+ORDER BY    ColumnName
+
+
+
+ 
 
 
             
