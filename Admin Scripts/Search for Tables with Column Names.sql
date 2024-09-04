2f6601222609a3e085e7ba351753cd1c9302895f
Michael DeLisio <mdelisio@terrasmart.com>
2024-09-04 17:06:21 -0400

    column search

--- Admin Scripts/Search for Tables with Column Names.sql
+++ Admin Scripts/Search for Tables with Column Names.sql
@@ -20,8 +20,11 @@ SELECT      COLUMN_NAME AS 'ColumnName'
             ,TABLE_NAME AS  'TableName'
             ,TABLE_SCHEMA as 'SchemaName'
 FROM        INFORMATION_SCHEMA.COLUMNS
-WHERE       COLUMN_NAME LIKE 'Vendor%'
+WHERE       COLUMN_NAME LIKE 'Jobseq%'
+--AND         TABLE_Name LIke '%PO%'
+and         Table_Schema = 'ODS_ERPDB'
 ORDER BY    ColumnName,TableName
+
 
 
 
